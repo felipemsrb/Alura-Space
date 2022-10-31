ac215feef4ea0112f7e525354827c6dc5efba589
Felipe Mota <felipe.msrb@gmail.com>
2022-10-31 10:02:27 +0000

    criando modulos para personalizar os componentes

--- src/Componentes/Cabecalho/index.js
+++ src/Componentes/Cabecalho/index.js
@@ -1,13 +1,16 @@
-import "./Cabecalho.css";
+import styles from "./Cabecalho.module.scss";
 import search from "./search.png";
 import logo from "./logo.png";
 
 const Cabecalho = () => {
   return (
-    <header className="cabecalho">
+    <header className={styles.cabecalho}>
       <img src={logo} alt="Logo do site"></img>
-      <div className="cabecalho__container">
-        <input placeholder="O que você procura?"></input>
+      <div className={styles.cabecalho__container}>
+        <input
+          className={styles.cabecalho__input}
+          placeholder="O que você procura?"
+        ></input>
         <img src={search} alt="Imagem da lupa para pesquisa"></img>
       </div>
     </header>
