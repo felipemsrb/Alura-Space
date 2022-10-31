4326b1c039caf5af19f28266119c1fc394aa1fd8
Felipe Mota <felipe.msrb@gmail.com>
2022-10-31 10:32:05 +0000

    importando imagens para o componente menu

--- src/Componentes/Menu/index.js
+++ src/Componentes/Menu/index.js
@@ -1,5 +1,37 @@
+import styles from "./Menu.module.scss";
+import home from "./home-ativo.png";
+import curtidas from "./mais-curtidas-inativo.png";
+import vistas from "./mais-vistas-inativo.png";
+import novas from "./novas-inativo.png";
+import surpreendame from "./surpreenda-me-inativo.png";
+
 const Menu = () => {
-  return <h1>Menu</h1>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <img src={home} alt="Imagem"></img>
+          <a href="/">Início</a>
+        </li>
+        <li>
+          <img src={curtidas} alt="Imagem"></img>
+          <a href="/">Mais curtidas</a>
+        </li>
+        <li>
+          <img src={vistas} alt="Imagem"></img>
+          <a href="/">Mais vistas</a>
+        </li>
+        <li>
+          <img src={novas} alt="Imagem"></img>
+          <a href="/">Novas</a>
+        </li>
+        <li>
+          <img src={surpreendame} alt="Imagem"></img>
+          <a href="/">Surpreenda-me</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Menu;
